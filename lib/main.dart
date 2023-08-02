b67125413de51f1ed31ac5d0b8ad7aec1139338d
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2023-08-02 22:21:28 +0530

    added: confetti, theme colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 
 import './constants.dart';
@@ -34,11 +37,23 @@ class _HomePageState extends State<HomePage> {
   late MediaQueryData media;
   late double deviceWidth;
   late double deviceHeight;
+  late Map<String, Widget> iconMap;
+  late ThemeData theme;
+  late ConfettiController _controllerCenter;
 
   @override
   void initState() {
     super.initState();
+    _controllerCenter = ConfettiController(
+      duration: const Duration(seconds: 2),
+    );
     _initialize();
+  }
+
+  @override
+  void dispose() {
+    _controllerCenter.dispose();
+    super.dispose();
   }
 
   void _initialize() {
@@ -56,6 +71,31 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  Path drawStar(Size size) {
+    // Method to convert degree to radians
+    double degToRad(double deg) => deg * (pi / 180.0);
+
+    const numberOfPoints = 5;
+    final halfWidth = size.width / 2;
+    final externalRadius = halfWidth;
+    final internalRadius = halfWidth / 2.5;
+    final degreesPerStep = degToRad(360 / numberOfPoints);
+    final halfDegreesPerStep = degreesPerStep / 2;
+    final path = Path();
+    final fullAngle = degToRad(360);
+    path.moveTo(size.width, halfWidth);
+
+    for (double step = 0; step < fullAngle; step += degreesPerStep) {
+      path.lineTo(halfWidth + externalRadius * cos(step),
+          halfWidth + externalRadius * sin(step));
+      path.lineTo(halfWidth + internalRadius * cos(step + halfDegreesPerStep),
+          halfWidth + internalRadius * sin(step + halfDegreesPerStep));
+    }
+    path.close();
+
+    return path;
+  }
+
   String _getSymbol() {
     String currSymbol = symbol;
 
@@ -70,16 +110,38 @@ class _HomePageState extends State<HomePage> {
     return currSymbol;
   }
 
+  _initializeSymbols() {
+    iconMap = {
+      Constants.symbolX: Icon(
+        Icons.close,
+        size: deviceWidth < deviceHeight
+            ? deviceWidth * 0.15
+            : deviceHeight * 0.15,
+        color: theme.colorScheme.primary,
+      ),
+      Constants.symbolO: Icon(
+        Icons.radio_button_off_rounded,
+        size: deviceWidth < deviceHeight
+            ? deviceWidth * 0.13
+            : deviceHeight * 0.13,
+        color: theme.colorScheme.secondary,
+      ),
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     media = MediaQuery.of(context);
     deviceWidth = media.size.width;
     deviceHeight = media.size.height;
+    theme = Theme.of(context);
+
+    _initializeSymbols();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Tic-Tac-Toe'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: theme.colorScheme.inversePrimary,
         actions: [
           IconButton(
             tooltip: 'Restart Game',
@@ -93,51 +155,96 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: SafeArea(
-        child: Center(
-          child: SizedBox(
-            width: deviceWidth < deviceHeight
-                ? deviceWidth * 0.6
-                : deviceHeight * 0.6,
-            child: GridView.count(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              crossAxisCount: Constants.size,
-              children: List.generate(
-                Constants.size * Constants.size,
-                (index) => Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(),
-                  ),
-                  child: InkWell(
-                    onTap: !isGameOver && symbolList[index].trim().isEmpty
-                        ? () {
-                            setState(() {
-                              currentIdx = index;
-                            });
+        child: Stack(
+          children: [
+            Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(
+                      width: deviceWidth < deviceHeight
+                          ? deviceWidth * 0.7
+                          : deviceHeight * 0.6,
+                      child: GridView.count(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        crossAxisCount: Constants.size,
+                        children: List.generate(
+                          Constants.size * Constants.size,
+                          (index) => Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(),
+                            ),
+                            child: InkWell(
+                              onTap: !isGameOver &&
+                                      symbolList[index].trim().isEmpty
+                                  ? () {
+                                      setState(() {
+                                        currentIdx = index;
+                                      });
 
-                            symbolList[index] = _getSymbol();
+                                      symbolList[index] = _getSymbol();
 
-                            if (_isGameOver()) {
-                              _showSnackBar(context, symbolList[currentIdx]);
-                              return;
-                            }
-                          }
-                        : null,
-                    child: Center(
-                      child: Text(
-                        symbolList[index],
-                        style: TextStyle(
-                          fontSize: deviceWidth < deviceHeight
-                              ? deviceWidth * 0.1
-                              : deviceHeight * 0.1,
+                                      if (_isGameOver()) {
+                                        _showSnackBar(
+                                            context, symbolList[currentIdx]);
+                                        return;
+                                      }
+                                    }
+                                  : null,
+                              child: Center(
+                                child: symbolList[index] == ''
+                                    ? null
+                                    : iconMap[symbolList[index]],
+                              ),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: deviceWidth < deviceHeight
+                            ? deviceWidth * 0.04
+                            : deviceHeight * 0.04,
+                      ),
+                      child: Center(
+                        child: Text(
+                          isGameOver
+                              ? 'The Winner is ${symbolList[currentIdx]}!'
+                              : '',
+                          style: TextStyle(
+                            fontSize: deviceWidth < deviceHeight
+                                ? deviceWidth * 0.08
+                                : deviceHeight * 0.08,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ),
+            Align(
+              alignment: Alignment.center,
+              child: ConfettiWidget(
+                confettiController: _controllerCenter,
+                blastDirectionality: BlastDirectionality
+                    .explosive, // don't specify a direction, blast randomly
+                shouldLoop:
+                    true, // start again as soon as the animation is finished
+                colors: const [
+                  Colors.green,
+                  Colors.blue,
+                  Colors.pink,
+                  Colors.orange,
+                  Colors.purple
+                ], // manually specify the colors to be used
+                createParticlePath: drawStar, // define a custom shape/path.
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -172,6 +279,10 @@ class _HomePageState extends State<HomePage> {
         _secondaryDiagonalWin(row, col, symbolList[currentIdx])) {
       setState(() {
         isGameOver = true;
+        _controllerCenter.play();
+        Future.delayed(const Duration(seconds: 3)).then(
+          (value) => _controllerCenter.stop(),
+        );
       });
       return true;
     }
